9b3e7bc89282b6271e9747c26b11251ecdea5cce
Nish097 <nitya.oct21@gmail.com>
2025-05-08 15:13:58 +0530

    add some queries in report

--- CancerPatient_report.sql
+++ CancerPatient_report.sql
@@ -5,8 +5,16 @@ use cancer
 select * from cancer_patientData
 
 
-select count([Patient Id])  from cancer_patientData
+select count([Patient Id]) as high_risk_femalePatient from cancer_patientData
 where Gender=2 and Level='High'
 
-select count([Patient Id])  from cancer_patientData
+select count([Patient Id]) as High_risk_malePatient  from cancer_patientData
 where Gender=1 and Level='High'
+
+
+select [Alcohol use],smoking,[Dust Allergy]
+   from cancer_patientData
+   where [Alcohol use]=2 and [Dust Allergy]=4
+
+
+   
